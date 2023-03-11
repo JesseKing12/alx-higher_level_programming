6273ec53311693ddba4c85d8f62c0319eaf927d0
jesseking12 <ikechukwujesse3@gmail.com>
2023-03-11 23:01:02 +0100

    palindrome

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-
-# function that retrieves an element from a list like in C
 def element_at(my_list, idx):
-    if idx < 0:
-        return (none)
-    length = len(my_list)
-
-    if idx > length - 1
-        return (none)
-
-    return(my_list[idx])
+    return (my_list[idx] if 0 <= idx < len(my_list) else None)
